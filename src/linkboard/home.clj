ffd90d49655275190802d29f5bfa9584666502a3
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2024-11-28 02:58:53 +0000

    Fmt

--- src/linkboard/home.clj
+++ src/linkboard/home.clj
@@ -6,7 +6,8 @@
   [:div.flex.h-screen.flex-col.border-e.bg-white.max-w-xs
    [:div.inset-x-0.bottom-0.border-b.border-gray-100
     [:a.flex.items-center.gap-2.bg-white.p-4.hover:bg-gray-50 {:href "#"}
-     [:img.size-10.rounded-full.object-cover {:alt "" :src "https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"}]
+     [:img.size-10.rounded-full.object-cover {:alt ""
+                                              :src "https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"}]
      [:div
       [:p.text-xs
        [:strong.block.font-medium "Eric Frusciante"]]]]]
@@ -18,7 +19,6 @@
     [:ul.mt-2.space-y-1
      [:li
       [:a.block.rounded-lg.bg-gray-100.px-4.py-2.text-sm.font-medium.text-gray-700 {:href "#"} "General"]]]]])
-
 
 (defn home-view
   {:malli/schema [:=> [:cat] [:sequential :any]]}
