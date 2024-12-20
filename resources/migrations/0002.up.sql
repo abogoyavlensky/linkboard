a82dc415e03dc9b56e8913e7adc64a74c2f31271
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2024-12-20 03:00:56 +0000

    Fix migration

--- resources/migrations/0002.up.sql
+++ resources/migrations/0002.up.sql
@@ -1,10 +1,12 @@
+BEGIN TRANSACTION;
+--;;
 -- Create the user table
 CREATE TABLE "user" (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     sync_code VARCHAR NOT NULL UNIQUE,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
-
+--;;
 -- Create the board table
 CREATE TABLE "board" (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -12,9 +14,9 @@ CREATE TABLE "board" (
     user_id INTEGER NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE
 );
-
+--;;
 -- Create the link table
 CREATE TABLE "link" (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -24,5 +26,7 @@ CREATE TABLE "link" (
     board_id INTEGER NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (board_id) REFERENCES board (id) ON DELETE CASCADE
+    FOREIGN KEY (board_id) REFERENCES "board" (id) ON DELETE CASCADE
 );
+--;;
+COMMIT;
