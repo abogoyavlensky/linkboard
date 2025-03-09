5b94e95dde6bf1a82522818997140ac8c028f5bf
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-03-09 23:30:06 +0000

    Add script to fetch js assets

--- scripts/assets.clj
+++ scripts/assets.clj
@@ -1,10 +1,12 @@
 (ns assets
   (:require [clojure.string :as str]
             [babashka.fs :as fs]
-            [clj-commons.digest :as digest]))
+            [clj-commons.digest :as digest]
+            [babashka.http-client :as http]))
 
+(def ^:private ASSET-DIR-JS "resources/public/js")
 
-(defn hash-css!
+(defn hash-asset-file!
   [{:keys [asset-file target-file]}]
   (let [content (slurp asset-file)
         content-hash (digest/md5 content)
@@ -16,3 +18,59 @@
     (spit asset-file-path-hashed content)
     ; update output asset file name with hash in target file
     (fs/update-file target-file #(str/replace % asset-file-name asset-file-name-hashed))))
+
+(defn fetch-js!
+  "Fetches a JavaScript file from a URL and saves it to resources/public/js directory with version in the filename.
+   
+   Parameters:
+   - url: URL to fetch the JavaScript file from
+   - name: Base name for the file (without extension)
+   - version: Version string to include in the filename
+   - target-file: Optional file to update references in (similar to hash-css!)
+   - old-reference: Optional string to replace in target-file
+   
+   Returns the path to the saved file."
+  [{:keys [url-template filename-template version target-file old-reference]}]
+  (let [js-dir ASSET-DIR-JS
+        url (str/replace url-template #"\{\{VERSION\}\}" version)
+        filename (str/replace filename-template #"\{\{VERSION\}\}" version)
+        filepath (str js-dir "/" filename)]
+
+    ; Create js directory if it doesn't exist
+    (when-not (fs/exists? js-dir)
+      (fs/create-dirs js-dir))
+
+    ; Fetch the file and save it
+    (println (format "Fetching %s from %s" filename url))
+    (let [response (http/get url)
+          content (:body response)]
+      (if (= 200 (:status response))
+        (do
+          ; Save the file
+          (fs/create-dirs (fs/parent filepath))
+          (spit filepath content)
+          (println (format "Saved %s" filepath))
+          
+          ;; Update references in target file if provided
+          ;(when (and target-file old-reference)
+          ;  (fs/update-file target-file #(str/replace % old-reference filename)))
+          
+          filepath)
+        (throw (ex-info "Failed to fetch JavaScript file" 
+                       {:url url :status (:status response)}))))))
+
+
+
+(comment
+  (let [url "https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"
+        assets [{:url-template "https://cdn.jsdelivr.net/npm/alpinejs@{{VERSION}}/dist/cdn.min.js"
+                 :filename-template "alpinejs.{{VERSION}}.min.js"
+                 :version "3.14.8"}
+                {:url-template "https://cdn.jsdelivr.net/npm/@alpinejs/focus@{{VERSION}}/dist/cdn.min.js"
+                 :filename-template "alpinejs.focus.{{VERSION}}.min.js"
+                 :version "3.14.8"}
+                {:url-template "https://unpkg.com/htmx.org@{{VERSION}}/dist/htmx.min.js"
+                 :filename-template "htmx.{{VERSION}}.min.js"
+                 :version "2.0.4"}]]
+    (doseq [item assets]
+      (fetch-js! item))))
