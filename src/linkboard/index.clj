3188b35ddeae21bad1086367363ed56c77d5c890
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-03-14 14:06:22 +0000

    Update index

--- src/linkboard/index.clj
+++ src/linkboard/index.clj
@@ -7,17 +7,21 @@
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
     [:title "Clojure Stack Lite | A Template for Clojure Projects"]
     [:script {:src "https://cdn.tailwindcss.com"}]]
-   [:body.bg-slate-50.text-slate-800.min-h-screen.flex.flex-col
+   [:body.text-slate-800.min-h-screen.flex.flex-col
+    {:class ["bg-slate-50"]}
     [:nav.w-full.py-4.px-6.flex.justify-end
      [:a.text-slate-700.hover:text-slate-900 {:href "https://github.com/bogoyavlensky/clojure-stack-lite" :target "_blank" :rel "noopener noreferrer"}
       [:svg.h-6.w-6 {:xmlns "http://www.w3.org/2000/svg" :fill "currentColor" :viewBox "0 0 24 24"}
        [:path {:d "M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"}]]]]
     [:main.flex-grow.flex.items-center.justify-center
      [:div.container.mx-auto.px-4.max-w-4xl.text-center
-      [:h1.text-6xl.font-bold.mb-6.text-slate-900 "Welcome to " [:span.bg-gradient-to-r.from-emerald-400.via-sky-500.to-sky-300.bg-clip-text.text-transparent.relative "Clojure
+      [:h1.text-6xl.font-bold.mb-6.text-slate-900 "Welcome to " [:span.bg-gradient-to-r.from-emerald-400.via-sky-400.to-sky-200.bg-clip-text.text-transparent.relative "Clojure
           Stack Lite"]]
-      [:p.text-xl.mb-10.text-slate-500 "A lightweight, modern template to jumpstart your Clojure projects"]
-      [:p.text-lg.mb-12.text-slate-600 "To begin, add new routes in " [:code.bg-slate-200.px-1.rounded "src/yourproject/routes.clj"] " and define a handler in " [:code.bg-slate-200.px-1.rounded "src/yourproject/handlers.clj"]]
+      [:p.text-2xl.mb-10.text-slate-500 "A lightweight, modern template to jumpstart your Clojure projects"]
+      [:p.text-lg.mb-12.text-slate-600
+       "To begin, just change this view at " [:code.bg-slate-200.px-1.rounded "src/yourproject/views/home.clj"]
+       " or add new route in " [:code.bg-slate-200.px-1.rounded "src/yourproject/routes.clj"]
+       " and define a handler in " [:code.bg-slate-200.px-1.rounded "src/yourproject/handlers.clj"]]
       [:div.mb-16
        [:a.bg-slate-900.hover:bg-slate-800.text-white.font-medium.py-3.px-8.rounded-lg.transition-colors.duration-200.mr-4 {:href "https://github.com/bogoyavlensky/clojure-stack-lite"} "Get Started"]
        [:a.bg-white.hover:bg-slate-100.text-slate-900.font-medium.py-3.px-8.rounded-lg.border.border-slate-300.transition-colors.duration-200 {:href "https://github.com/bogoyavlensky/clojure-stack-lite/docs"} "Documentation"]]
