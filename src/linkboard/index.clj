6618537f156a4f60c33efc0ec6f3c179a458ef8f
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-03-15 15:30:54 +0000

    Change subtitle color on index page

--- src/linkboard/index.clj
+++ src/linkboard/index.clj
@@ -65,7 +65,7 @@
        [:h1 {:class ["text-6xl" "font-bold" "mb-6" "text-slate-900"]} "Welcome to "
         [:span {:class ["bg-gradient-to-r" "from-emerald-400" "via-sky-400" "to-sky-200" "bg-clip-text" "text-transparent" "relative"]}
          "Clojure Stack Lite"]]
-       [:p {:class ["text-2xl" "mb-10" "text-slate-500"]} "A lightweight, modern template to jumpstart your Clojure projects"]
+       [:p {:class ["text-2xl" "mb-10" "text-slate-600"]} "A lightweight, modern template to jumpstart your Clojure projects"]
        [:p {:class ["text-lg" "mb-12" "text-slate-500"]}
         "To begin, modify the existing view in " [:code {:class ["bg-slate-100" "px-1" "rounded"]} "src/yourproject/views/home.clj"]
         " or add a new route in " [:code {:class ["bg-slate-100" "px-1" "rounded"]} "src/yourproject/routes.clj"]
