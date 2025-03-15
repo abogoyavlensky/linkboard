f5fd47a787bbf47cfa4522c868c5e9bd9b7cb3ec
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-03-15 11:58:09 +0000

    Update code block style on in dex page

--- src/linkboard/index.clj
+++ src/linkboard/index.clj
@@ -64,9 +64,9 @@
          "Clojure Stack Lite"]]
        [:p {:class ["text-2xl" "mb-10" "text-slate-500"]} "A lightweight, modern template to jumpstart your Clojure projects"]
        [:p {:class ["text-lg" "mb-12" "text-slate-500"]}
-        "To begin, modify the existing view in " [:code {:class ["bg-slate-200" "px-1" "rounded"]} "src/yourproject/views/home.clj"]
-        " or add a new route in " [:code {:class ["bg-slate-200" "px-1" "rounded"]} "src/yourproject/routes.clj"]
-        " and define a handler in " [:code {:class ["bg-slate-200" "px-1" "rounded"]} "src/yourproject/handlers.clj"]]
+        "To begin, modify the existing view in " [:code {:class ["bg-slate-100" "px-1" "rounded"]} "src/yourproject/views/home.clj"]
+        " or add a new route in " [:code {:class ["bg-slate-100" "px-1" "rounded"]} "src/yourproject/routes.clj"]
+        " and define a handler in " [:code {:class ["bg-slate-100" "px-1" "rounded"]} "src/yourproject/handlers.clj"]]
        [:div {:class ["mb-16"]}
         [:a {:class ["bg-slate-900" "hover:bg-slate-800" "text-white" "font-medium" "py-3" "px-8" "rounded-lg" "transition-colors" "duration-200" "mr-4"]
              :href "https://github.com/bogoyavlensky/clojure-stack-lite"} "Get Started"]
