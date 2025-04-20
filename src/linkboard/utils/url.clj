7c56708454144cf807bc771e626f9faae364e81e
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-04-20 23:05:38 +0100

    Simplify fetching url

--- src/linkboard/utils/url.clj
+++ src/linkboard/utils/url.clj
@@ -1,12 +1,9 @@
 (ns linkboard.utils.url
   (:require [clojure.string :as str]
+            [clj-http.client :as http]
+            [lambdaisland.uri :as uri]
             [hickory.core :as hickory]
-            [hickory.select :as s])
-  (:import (java.io ByteArrayOutputStream)
-           (java.net HttpURLConnection URI URL)
-           (java.security SecureRandom)
-           (java.security.cert X509Certificate)
-           (javax.net.ssl HttpsURLConnection SSLContext TrustManager X509TrustManager HostnameVerifier)))
+            [hickory.select :as s]))
 
 (def ^:private max-download-bytes
   "Maximum number of bytes to download (1MB)"
@@ -16,99 +13,51 @@
   "Extract domain from URL for fallback metadata."
   [url]
   (try
-    (let [uri (URI. url)
-          host (.getHost uri)]
+    (let [parsed (uri/uri url)
+          host (or (:host parsed) "")]
       (if (str/blank? host)
         url
-        (let [domain (str/replace host #"^www\." "")]
-          domain)))
+        (str/replace host #"^www\." "")))
     (catch Exception _
+      ; If URL parsing fails, return the original URL
       (or
         (second (re-find #"^(?:https?://)?(?:www\.)?([^/]+)" url))
         url))))
-
-(defn- setup-trust-all-certs
-  "Configure connection to accept all SSL certificates to avoid failures."
-  []
-  (let [trust-all-certs (into-array TrustManager
-                                    [(proxy [X509TrustManager] []
-                                       (getAcceptedIssuers [] (make-array X509Certificate 0))
-                                       (checkClientTrusted [_ _])
-                                       (checkServerTrusted [_ _]))])
-        ssl-context (SSLContext/getInstance "TLS")]
-    (.init ssl-context nil trust-all-certs (SecureRandom.))
-    (HttpsURLConnection/setDefaultSSLSocketFactory (.getSocketFactory ssl-context))
-    (HttpsURLConnection/setDefaultHostnameVerifier
-      (proxy [HostnameVerifier] []
-        (verify [_ _] true)))))
 
 (defn- fetch-url-limited
   "Fetch URL content with a size limit to prevent downloading large files."
   [url-str]
   (try
-    (setup-trust-all-certs)
-    (let [url (URL. url-str)
-          conn ^HttpURLConnection (.openConnection url)]
-      (.setRequestProperty conn "User-Agent" "Mozilla/5.0 (compatible; LinkBoard/1.0)")
-      (.setConnectTimeout conn 5000)
-      (.setReadTimeout conn 5000)
-      (when (.getHeaderField conn "Content-Type")
-        (let [content-type (.toLowerCase (.getHeaderField conn "Content-Type"))]
-          (when-not (str/includes? content-type "text/html")
-            ;; If not HTML, don't download
-            (throw (ex-info "Not an HTML page" {:url url-str
-                                                :content-type content-type})))))
-
-      ;; Read with size limit
-      (with-open [is (.getInputStream conn)
-                  bos (ByteArrayOutputStream.)]
-        (let [buffer (byte-array 4096)
-              result (loop [total-bytes 0]
-                       (let [bytes-read (.read is buffer)]
-                         (cond
-                           ;; EOF
-                           (neg? bytes-read)
-                           {:html (.toString bos "UTF-8")
-                            :content-type (.getHeaderField conn "Content-Type")}
-
-                           ;; Size limit reached
-                           (> (+ total-bytes bytes-read) max-download-bytes)
-                           (do
-                             (.write bos buffer 0 (- max-download-bytes total-bytes))
-                             {:html (.toString bos "UTF-8")
-                              :truncated true
-                              :content-type (.getHeaderField conn "Content-Type")})
-
-                           ;; Continue reading
-                           :else
-                           (do
-                             (.write bos buffer 0 bytes-read)
-                             (recur (+ total-bytes bytes-read))))))]
-          result)))
+    (let [response (http/get url-str
+                     {:socket-timeout 5000
+                      :conn-timeout 5000
+                      :max-body-length max-download-bytes
+                      :headers {"User-Agent" "Mozilla/5.0 (compatible; LinkBoard/1.0)"}
+                      :insecure? true      ; Accept self-signed certificates
+                      :throw-exceptions false})]
+      
+      (if (not= 200 (:status response))
+        {:error (str "HTTP error: " (:status response))
+         :url url-str}
+        (let [content-type (get-in response [:headers "content-type"] "")]
+          (if-not (str/includes? (str/lower-case content-type) "text/html")
+            {:error (str "Not an HTML page: " content-type)
+             :url url-str}
+            {:html (:body response)
+             :content-type content-type}))))
     (catch Exception e
       {:error (str "Error fetching URL: " (.getMessage e))
        :url url-str})))
 
-(defn- normalize-url
+(defn- get-favicon-url
   "Normalize URL for favicon handling."
   [base-url path]
-  (cond
-    ;; Already absolute URL
-    (re-find #"^https?://" path) path
+  (let [url-map (uri/uri base-url)
+        favicon-path (if (str/starts-with? path "/")
+                       path
+                       (str "/" path))]
+    (format "%s://%s%s" (:scheme url-map) (:host url-map) favicon-path)))
 
-    ;; Protocol-relative URL
-    (re-find #"^//" path) (str (if (str/starts-with? base-url "https") "https:" "http:") path)
-
-    ;; Root-relative URL
-    (str/starts-with? path "/")
-    (let [url-obj (URL. base-url)
-          base (str (.getProtocol url-obj) "://" (.getHost url-obj))]
-      (str base path))
-
-    ;; Relative URL
-    :else
-    (let [base-url (if (str/ends-with? base-url "/") base-url (str base-url "/"))]
-      (str base-url path))))
 
 (defn- parse-html-metadata
   "Extract title and favicon from HTML content."
@@ -138,9 +87,8 @@
                       (get-in (first icon-nodes) [:attrs :href]))
 
           ;; Normalize icon URL or default to /favicon.ico
-          favicon-url (if (and icon-href (not (str/blank? icon-href)))
-                        (normalize-url url icon-href)
-                        (normalize-url url "/favicon.ico"))]
+          favicon-url (when (and icon-href (not (str/blank? icon-href)))
+                        (get-favicon-url url icon-href))]
 
       {:title (if (str/blank? title)
                 (get-domain-from-url url)
