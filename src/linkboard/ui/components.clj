1f89c46bb3f2a43d4c32887d49eb544100b6e54e
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-04-20 21:20:50 +0100

    Fix close button and modal background

--- src/linkboard/ui/components.clj
+++ src/linkboard/ui/components.clj
@@ -84,9 +84,7 @@
   [{:keys [title open-btn-text submit-btn-title form-attrs form-fields]}]
   [:div.relative.w-auto.h-auto
    {:x-data "{ modalOpen: false }"
-    :x-on:keydown.escape.window "modalOpen = false"
-    ::class "{ 'z-40': modalOpen }"}
-    ;:class ["relative" "w-auto" "h-auto"]}
+    :x-on:keydown.escape.window "modalOpen = false"}
    [:button
     {:x-on:click "modalOpen=true"
      :class "focus:ring-neutral-200/60"}
@@ -96,7 +94,7 @@
      :x-cloak "true"
      :class ["z-99" "fixed" "top-0" "left-0" "flex" "items-center" "justify-center" "w-screen" "h-screen"]}
     [:div
-     {:class ["absolute" "inset-0" "w-full" "h-full" "bg-white" "backdrop-blur-xs" "bg-opacity-70"]
+     {:class ["absolute" "inset-0" "w-full" "h-full" "backdrop-blur-xs" "bg-opacity-70" "bg-black/50"]
       :x-show "modalOpen"
       :x-transition:enter "ease-out duration-300"
       :x-transition:enter-start "opacity-0"
@@ -106,7 +104,8 @@
       :x-transition:leave-end "opacity-0"
       :x-on:click "modalOpen=false"}]
     [:form
-     (merge {:class ["relative" "w-full" "py-6" "bg-white" "border" "shadow-lg" "px-7" "border-neutral-200" "max-w-xs" "md:max-w-md" "rounded-lg"]
+     (merge {:class ["relative" "w-full" "py-6" "bg-white" "border" "shadow-lg" "px-7"
+                     "border-neutral-200" "max-w-xs" "md:max-w-md" "rounded-lg"]
              :x-show "modalOpen"
              :x-trap.inert.noscroll "modalOpen"
              :x-transition:enter "ease-out duration-300"
@@ -118,8 +117,9 @@
             form-attrs)
      [:div {:class ["flex" "items-center" "justify-between" "pb-3"]}
       [:h3 {:class ["text-lg" "font-semibold"]} title]
-      [:button
-       {:class ["absolute" "top-0" "right-0" "flex" "items-center" "justify-center" "w-8" "h-8" "mt-5" "mr-5" "text-gray-600" "rounded-full" "hover:text-gray-800" "hover:bg-gray-50"]
+      [:div
+       {:class ["absolute" "top-0" "right-0" "flex" "items-center" "justify-center"
+                "w-8" "h-8" "mt-5" "mr-5" "text-gray-600" "rounded-full" "hover:text-gray-800" "hover:bg-gray-50"]
         :x-on:click "modalOpen=false"}
        [:svg {:class ["w-5" "h-5"]
               :xmlns "http://www.w3.org/2000/svg"
@@ -139,11 +139,14 @@
      [:div
       {:class ["flex" "flex-row" "justify-end" "space-x-2"]}
       [:button
-       {:class ["inline-flex" "items-center" "justify-center" "h-10" "px-4" "py-2" "text-sm" "font-medium" "transition-colors" "border" "rounded-md" "focus:outline-hidden" "focus:ring-2" "focus:ring-neutral-100" "focus:ring-offset-2"]
+       {:class ["inline-flex" "items-center" "justify-center" "h-10" "px-4" "py-2"
+                "text-sm" "font-medium" "transition-colors" "border" "rounded-md"
+                "focus:outline-hidden" "focus:ring-2" "focus:ring-neutral-100" "focus:ring-offset-2"]
         :x-on:click "modalOpen=false"
         :type "button"} "Cancel"]
       [:button
-       {:class ["inline-flex" "items-center" "justify-center" "px-4" "py-2" "bg-blue-600" "text-white" "rounded-lg" "hover:bg-blue-700" "transition-colors"]
+       {:class ["inline-flex" "items-center" "justify-center" "px-4" "py-2"
+                "bg-blue-600" "text-white" "rounded-lg" "hover:bg-blue-700" "transition-colors"]
         :x-on:click "modalOpen=false"
         :type "submit"}
        (or submit-btn-title "Save")]]]]])
