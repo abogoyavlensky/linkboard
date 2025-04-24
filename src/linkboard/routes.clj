64250b2b259782e3cd65d30f86b0a5946aa5ce91
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-04-25 00:00:27 +0100

    Add sync code to the session

--- src/linkboard/routes.clj
+++ src/linkboard/routes.clj
@@ -1,15 +1,34 @@
 (ns linkboard.routes
-  (:require [linkboard.board.handlers :as board-handlers]
+  (:require [clojure.string :as str]
+            [linkboard.board.handlers :as board-handlers]
             [linkboard.home.handlers :as home-handlers]
+            [clj-ulid :as ulid]
             [ring.util.response :as response]))
+
+(defn wrap-sync-code
+  "Middleware that manages the sync-code in session.
+   - Adds a random UUID as :sync-code to request if not present in session
+   - Transfers :sync-code from request to session in response if present"
+  [handler]
+  (fn [request]
+    (let [session (:session request)
+          has-sync-code? (boolean (:sync-code session))
+          updated-request (if has-sync-code?
+                            request
+                            (assoc-in request [:session :sync-code] (str/upper-case (ulid/ulid))))
+          response (handler updated-request)]
+      (if (not has-sync-code?)
+        (assoc response :session {:sync-code (get-in updated-request [:session :sync-code])})
+        response))))
 
 (def routes
   [["/" {:name ::home-page
+         :middleware [wrap-sync-code]
          :get {:handler home-handlers/home-handler
                :responses {200 {:body string?}}}}]
    ["/up" {:name ::health-check
            :get {:handler (fn [_] (response/response "OK"))}}]
-   ["/boards"
+   ["/boards" {:middleware [wrap-sync-code]}
     ["" {:name ::board-list
          :post {:handler home-handlers/create-board-handler
                 :parameters {:form {:title [:string {:min 1}]}}
