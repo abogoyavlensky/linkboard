66fd3b89ea9203c970cc16bfa4c107482d9725ac
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-08-07 00:48:19 +0100

    fmt

--- src/linkboard/spec.clj
+++ src/linkboard/spec.clj
@@ -2,7 +2,7 @@
   (:require [lambdaisland.uri :as uri]))
 
 (def Link [:and [:string {:min 1}]
-                [:fn {:error/message "must be a valid URL"}
-                 #(try
-                    (uri/uri %)
-                    (catch Exception _ false))]])
+           [:fn {:error/message "must be a valid URL"}
+            #(try
+               (uri/uri %)
+               (catch Exception _ false))]])
