717bd6f3dd2bf6a88016cea6de88014d48e73889
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-08-16 01:19:41 +0100

    fmt

--- src/linkboard/ui/icons.clj
+++ src/linkboard/ui/icons.clj
@@ -43,7 +43,9 @@
 
 (def chevron-left
   (base-svg
-   {:path {:stroke-linecap "round" :stroke-linejoin "round" :d "M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"}}))
+    {:path {:stroke-linecap "round"
+            :stroke-linejoin "round"
+            :d "M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"}}))
 
 (def queue-list
   (base-svg
