b4040468e2d47cda324b3cd81847b71871e1d396
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-08-23 02:09:21 +0100

    fmt

--- src/linkboard/queries.clj
+++ src/linkboard/queries.clj
@@ -179,12 +179,12 @@
                                           :where [:and
                                                   [:= :l.user-id user-id]
                                                   [:raw "link_search MATCH " [search-term]]]
-                                          :order-by [[:search-rank :asc][:l.favorite :desc][:l.created-at :desc]])
+                                          :order-by [[:search-rank :asc] [:l.favorite :desc] [:l.created-at :desc]])
     (< (count raw-search-term) 3) (assoc :select [:l.* [:b.title :board-title] [:b.id :board-id]]
                                          :where [:and
                                                  [:= :l.user-id user-id]
                                                  [:like :l.title (str "%" raw-search-term "%")]]
-                                         :order-by [[:l.favorite :desc][:l.created-at :desc]])))
+                                         :order-by [[:l.favorite :desc] [:l.created-at :desc]])))
 
 (defn search-board-links-query
   "Build query for searching links within a specific board using FTS5 or LIKE for short terms.
@@ -200,14 +200,14 @@
                                                   [:= :b.id board-id]
                                                   [:= :b.user-id user-id]
                                                   [:raw "link_search MATCH " [search-term]]]
-                                          :order-by [[:search-rank :asc][:l.favorite :desc][:l.created-at :desc]])
+                                          :order-by [[:search-rank :asc] [:l.favorite :desc] [:l.created-at :desc]])
     (< (count raw-search-term) 3) (assoc :select [:l.*]
                                          :where [:and
                                                  [:= :l.user-id user-id]
                                                  [:= :b.id board-id]
                                                  [:= :b.user-id user-id]
                                                  [:like :l.title (str "%" raw-search-term "%")]]
-                                         :order-by [[:l.favorite :desc][:l.created-at :desc]])))
+                                         :order-by [[:l.favorite :desc] [:l.created-at :desc]])))
 
 (defn get-all-links-query
   "Build query for getting all user links, optionally with search.
@@ -219,7 +219,7 @@
      :from [[:link :l]]
      :left-join [[:board :b] [:= :l.board-id :b.id]]
      :where [:= :l.user-id user-id]
-     :order-by [[:l.favorite :desc][:l.created-at :desc]]}))
+     :order-by [[:l.favorite :desc] [:l.created-at :desc]]}))
 
 (defn get-board-links-query
   "Build query for getting links within a specific board, optionally with search.
@@ -233,7 +233,7 @@
      :where [:and
              [:= :b.user-id user-id]
              [:= :b.id board-id]]
-     :order-by [[:l.favorite :desc][:l.created-at :desc]]}))
+     :order-by [[:l.favorite :desc] [:l.created-at :desc]]}))
 
 (comment
   ; Test search preprocessing
