05f36335ea9d00465416e9d13af812b868099b8f
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-08-30 01:04:19 +0100

    Improve registration messages

--- src/linkboard/ui/components.clj
+++ src/linkboard/ui/components.clj
@@ -259,7 +259,20 @@
                       [:div {:class ["ml-3"]}
                        [:h3 {:class ["text-sm" "font-medium" "text-red-800"]} "Warning"]
                        [:div {:class ["mt-2" "text-sm" "text-red-700"]}
-                        [:p "Please store account number safely - you cannot restore your account if it is lost."]]]]]]]}))
+                        [:p "Please store account number safely. It " [:strong "won't be shown again"] " and is required for login."]]]]]
+                    [:div {:class ["bg-green-50" "border" "border-green-200" "rounded-lg" "p-4" "mt-4"]}
+                     [:div {:class ["flex" "items-start"]}
+                      [:div {:class ["flex-shrink-0"]}
+                       [:svg {:class ["h-5" "w-5" "text-green-400"]
+                              :viewBox "0 0 20 20"
+                              :fill "currentColor"}
+                        [:path {:fill-rule "evenodd"
+                                :d "M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                :clip-rule "evenodd"}]]]
+                      [:div {:class ["ml-3"]}
+                       [:h3 {:class ["text-sm" "font-medium" "text-green-800"]} "Info"]
+                       [:div {:class ["mt-2" "text-sm" "text-green-700"]}
+                        [:p "Your data from current temporary session will be transferred to your new account."]]]]]]]}))
 
 (defn- help-modal
   []
